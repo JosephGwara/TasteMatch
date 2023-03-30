b81b3874c3eb5bd98f0d9c98ccc8e4b3167876a8
Joseph Gwara <jgwarix@gmail.com>
2023-03-30 20:49:56 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tastematch/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tastematch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
